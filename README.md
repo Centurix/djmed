0e23f7d516088a288e9d58a98c8f2f60bb36da6e
Chris <centurix@gmail.com>
2025-09-13 12:33:13 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+DJMed and Yuwell CPAP Results Reader
+====================================
+
+![JDMed/Yuwell CPAP Machine](./examples/djmed.png)
+
+Extract charts like this
+
+![DJMed/Yuwell Chart](./examples/chart.png)
+
+
+WARNING: This is very much a work-in-progress, but it does produce usable charts from sleep data. 
+
+**DISCLAIMER: THIS SHOULD NOT BE USED FOR ANY DIAGNOSTIC PURPOSES**
+
+This parses and presents data from a DJMed/Yuwell CPAP/APAP machine. These machines store data on SD Card in session files ending with the `.bys` extension.
+
+This is a binary grammar. The `010` editor grammar support file is included in the doc directory.
+
+I am still figuring out the binary grammar, particularly the flags in each record, which could be any of the following:
+
+* CSR = Cheyne-Stokes Respiration, heart disease related breathing
+* LL = Large Leak
+* CA = Central Apnea
+* OA = Obstructive Apnea
+* H = Hypopnea
+* UA = Unknown Apnea
+* RE = RERA = Respiratory Effort Related Arousal
+
+Any information about these flags and locations would be very welcome.
+
+Installation and Running
+------------------------
+
+Right now, this will show charts for all files found in the `data` directory of the repo. This uses `uv` for package management. But it only uses `matplotlib` and `numpy` for the charts. This was developed using Python 3.14, but it should work in 3.7+.
+
+1. Clone this repository
+2. Install the dependencies with either `uv sync` or `pip install matplotlib numpy`
+3. Copy your `.bys` files into the `data` directory directly, no sub-directories
+4. Run `python main.py`
+
+I've found these as re-branded units from many companies and drop-shipping establishments. The base manufacturer is Yuwell and this model appears to be a YH550.
+
+BYS File Format
+---------------
+
+The file consists of two main sections:
+
+1. File header
+2. Log records
+
+The file header contains fixed length fields and is 51 bytes in length. It includes machine settings and a summary of the logs. Most numeric fields are unsigned bytes.
+
+File header
+-----------
+
+Dates are formatted with a single byte for year, month, day, hour, minute and seconds.
+
+Years are 2 digit (25=2025 etc.)
+
+Decimal values are represented as the value multiplied by 10
+
+File is little endian
+
+
+| Offset | Length | Data |
+|--------|--------|------|
+| 0 | 6 | Log start date/time |
+| 6 | 6 | Log end date/time |
+| 12 | 1 | *unknown* |
+| 13 | 1 | Ramp up time (minutes, integer) |
+| 14 | 1 | Initial pressure (Decimal) |
+| 15 | 1 | Minimum pressure (Decimal) |
+| 16 | 1 | Maximum pressure (Decimal) |
+| 17 | 1 | *unknown* |
+| 18 | 1 | Humidity setting (integer) |
+| 19 | 7 | *unknown* |
+| 26 | 1 | Average leak volume (Decimal) |
+| 27 | 1 | *unknown* |
+| 28 | 1 | Average pressure (Decimal) |
+| 29 | 1 | *unknown* |
+| 30 | 16 | Product serial number |
+| 46 | 2 | Record count |
+| 48 | 2 | *unknown* (possibly related to record count) |
+| 50 | 1 | Header end marker (0xF9) |
+
+Log Records
+-----------
+
+Each record represents a minute of data containing the pressure level and leak level as well as event flags, most of which are currently not known. Each record is 10 bytes in length.
+
+According to the documentation, the file could contain SPo2 and pulse data. Which could be the fields with no data as my unit does not measure either.
+
+| Offset | Length | Data |
+|--------|--------|------|
+| 0 | 1 | Pressure (Decimal) |
+| 1 | 1 | *unknown* (No data) |
+| 2 | 1 | *unknown* (No data) |
+| 3 | 1 | *unknown* (Values seen are 0, 1) |
+| 4 | 1 | *unknown* (Values seen are 0, 1, 2. Possibly OSA event) |
+| 5 | 1 | *unknown* (Values seen are 0, 1, 2) |
+| 6 | 1 | *unknown* (Values seen are 0, 1) |
+| 7 | 1 | *unknown* (No data) |
+| 8 | 1 | *unknown* (No data) |
+| 9 | 1 | Leak volume (Decimal) |
+
+
+NOTE: The user manuals for the Yuwell units are included, there are no copyright terms in either documents.
---
+++ main.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+import os
+from pathlib import Path
+import struct
+from datetime import datetime, timedelta
+from decimal import Decimal
+from dataclasses import dataclass
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+"""
+Read DJMed CPAP files
+"""
+
+
+DATA = Path("data")
+
+                
+class InvalidCPAPFormat(Exception):
+    ...
+
+
+@dataclass
+class CPAPLogLine:
+    logged_time: datetime
+    pressure: Decimal
+    leakage: Decimal
+    u3: int
+    u4: int
+    u5: int
+    u6: int
+
+
+@dataclass
+class CPAPFile:
+    start: datetime
+    end: datetime
+    ramp_time: int
+    initial_pressure: Decimal
+    minimum_pressure: Decimal
+    maximum_pressure: Decimal
+    humidity: int
+    average_leak_volume: Decimal
+    average_pressure: Decimal
+    device_serial: str
+    logs: list[CPAPLogLine]
+
+    @classmethod
+    def from_file(cls, file_name: Path) -> "CPAPFile":
+        with open(file_name, "rb") as cpap_file:
+            start_year, start_month, start_day, start_hour, start_minute, start_second = struct.unpack("BBBBBB", cpap_file.read(6))
+            end_year, end_month, end_day, end_hour, end_minute, end_second = struct.unpack("BBBBBB", cpap_file.read(6))
+            unknown1 = cpap_file.read(1)
+            ramp_up_time, initial_pressure, minimum_pressure, maximum_pressure = struct.unpack("BBBB", cpap_file.read(4))
+            unknown2 = cpap_file.read(1)
+            humidity, = struct.unpack("B", cpap_file.read(1))
+            unknown3 = cpap_file.read(7)
+            average_leak_volume, = struct.unpack("B", cpap_file.read(1))
+            unknown4 = cpap_file.read(1)
+            average_pressure, = struct.unpack("B", cpap_file.read(1))
+            unknown5 = cpap_file.read(1)
+            serial, record_count = struct.unpack("16sh", cpap_file.read(18))
+            unknown6 = cpap_file.read(2)
+            unknown7, = struct.unpack("B", cpap_file.read(1))  # Always 0xF9?
+
+            if unknown7 != 0xF9:
+                raise InvalidCPAPFormat("Invalid CPAP format")
+
+            log_start = datetime(2000 + start_year, start_month, start_day, start_hour, start_minute, start_second)
+            log_end = datetime(2000 + end_year, end_month, end_day, end_hour, end_minute, end_second)
+
+            log_lines = []
+
+            for minute in range(record_count):
+                pressure, = struct.unpack("B", cpap_file.read(1))
+                u1, = struct.unpack("B", cpap_file.read(1))
+                u2, = struct.unpack("B", cpap_file.read(1))
+                u3, = struct.unpack("B", cpap_file.read(1))
+                u4, = struct.unpack("B", cpap_file.read(1))
+                u5, = struct.unpack("B", cpap_file.read(1))
+                u6, = struct.unpack("B", cpap_file.read(1))
+                u7, = struct.unpack("B", cpap_file.read(1))
+                u8, = struct.unpack("B", cpap_file.read(1))
+                leakage, = struct.unpack("B", cpap_file.read(1))
+
+                log_time = log_start + timedelta(minutes=minute)
+
+                # Always zero, one is possibly SPo2, another is the pulse rate
+                # If we find values, we're interested to know
+                if u1 + u2 + u7 + u8 > 0:
+                    raise InvalidCPAPFormat("Invalid CPAP format, unexpected data")
+
+                # Missing AH, HI events (possibly 1/0)
+                log_lines.append(CPAPLogLine(log_time, pressure / 10, leakage / 10, u3, u4, u5, u6))
+
+        return cls(
+            log_start,
+            log_end,
+            ramp_up_time,
+            initial_pressure / 10,
+            minimum_pressure / 10,
+            maximum_pressure / 10,
+            humidity,
+            average_leak_volume / 10,
+            average_pressure / 10,
+            serial,
+            log_lines
+        )
+
+
+def main():
+    """
+    For the time being, this just charts the minute logs in matplotlib
+    """
+    for file in os.scandir(DATA):
+        log_file = CPAPFile.from_file(file)
+        print(log_file.logs)
+        times = np.array([log.logged_time for log in log_file.logs])
+        pressures = np.array([log.pressure for log in log_file.logs])
+        leakages = np.array([log.leakage for log in log_file.logs])
+        u4 = np.array([log.u4 for log in log_file.logs])
+
+        fig, ax = plt.subplots(figsize=(30, 15))
+        ax.plot(times, pressures)
+        ax.plot(times, leakages)
+        ax.plot(times, u4)  # Marks increase in pressure?
+
+        locator = mdates.SecondLocator(interval=500)
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+
+        fig.autofmt_xdate()
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Pressure (cmH2O)")
+        ax.set_title(f"FILE: {file.name}: Starting {log_file.start.strftime('%Y-%m-%d')} (Between: {log_file.start.strftime('%H:%M')} and {log_file.end.strftime('%H:%M')}, Duration: {log_file.end - log_file.start})")
+        plt.grid()
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
